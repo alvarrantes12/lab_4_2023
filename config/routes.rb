028458945df267c8354ed8ff4563bcfc5617a109
Victoria <victoria.campos@ucrso.info>
2023-06-24 12:13:25 -0600

    VCF-C08854 API done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  scope module: :api do
+    resources :products
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"'
   root "dashboards#index"
 end
